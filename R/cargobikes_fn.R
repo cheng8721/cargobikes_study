f685a4757e8b6e59d92403afb15eed468d6e79ef
giacomodc <giacomodc@github.com>
2018-10-29 12:07:53 +0800

    - extended scheduling function to truck mode - TSP function simplified

--- R/cargobikes_fn.R
+++ R/cargobikes_fn.R
@@ -17,71 +17,124 @@ generate_demand <- function(data, xweight, simulated, ndel, nrep) {
   }
 }
 
-
-
-
-
-
-bike_assignment <- function(tmp_cut, tmp, boxweight, bike_tour_length, hub_centers) {
-  for (k in 1:max(tmp_cut)) {
-    if (all(is.na(tmp[which(tmp_cut==k),"tourID"]))) { #cluster not yet assigned
-      if (sum(tmp[which(tmp_cut==k),"weight"]) <= boxweight) { #tot cluster weight < bike capacity
-        if (nrow(tmp[which(tmp_cut==k),])>1) tourlength <- bike_tsp(tmp_tour=tmp[which(tmp_cut==k),], hub_centers)$tour_length else tourlength <- 0
-        if (tourlength <=  bike_tour_length) { #tot tour length < max allowed length
-          tourID <- ifelse(all(is.na(tmp$tourID)), 1, max(tmp$tourID, na.rm=T)+1)
-          tmp[which(tmp_cut==k),"tourID"] <- tourID #assign bike
-        }
-      }
-    }
+adjust_demand <- function(tmp, dwell_factor, max_weight) {
+### adjust demand according to given mode
+  # tmp
+  # dwell_factor
+  # max_weight
+###
+  
+  # adjust dwell time
+  tmp$dwell_time <- tmp$dwell_time*dwell_factor
+  
+  # adjust set of delivery points
+  while (any(tmp$weight>max_weight)) { #if the weight of a single delivery exceed mode capacity, the deliverty point is split in 2
+    excess_weight <- tmp[tmp$weight>max_weight,"weight"][1] - max_weight #find excess weight
+    tmp[nrow(tmp)+1,] <- tmp[tmp$weight>max_weight,][1,] #add new delivery point
+    tmp[tmp$weight>max_weight,"weight"][1] <- max_weight #adjust original del point to max capacity
+    tmp[nrow(tmp),"weight"] <- excess_weight #adjust weight of new delivery point
   }
-  return(tmp$tourID)
-  }
-
-
-
-
-
-bike_tsp <- function(tmp_tour, hub_centers) {
   
-  # compute distance matrix
-  dmat <- rbind(tmp_tour[,c("lon", "lat")], hub_centers[hub_centers$hub==unique(tmp_tour$hub),c("lon", "lat")])
-  dmat <- distm(dmat)
-  dmat <- as.dist(dmat) #in meters
-  tsp <- TSP(dmat, labels=c(tmp_tour$nodeID, "hub"))
-  
-  m <- as.matrix(tsp)
-  hub <- which(labels(tsp)=="hub")
-  
-  atsp <- ATSP(m[-hub,-hub])
-  atsp <- insert_dummy(atsp, label="HUB")
-  HUB <- which(labels(atsp)=="HUB")
-  atsp[HUB,] <- c(m[-hub,hub], 0)
-  atsp[,HUB] <- c(m[hub,-hub], 0)
-  
-  resTOUR <- solve_TSP(atsp, method="nearest_insertion")
-  
-  return(list("path_labels"=c("hub", labels(cut_tour(resTOUR, HUB)), "hub"), 
-              "tour_length"=tour_length(resTOUR)))
-  
+  return(tmp)
 }
 
 
 
 
-day_scheduling <- function(tmp, max_nboxes_hub, boxweight, bike_tour_length, hub_cut, avg_bike_speed, depot_loc) {
-###schedule delivery tours for bikes and assign them to hubs -------------------------------------
-  #tmp              = demand for single day
-  #max_nboxes_hub   = max no. of boxes carried by a hub
-  #boxweight        = max box weight
-  #bike_tour_length = max bike tour length
-  #hub_cut          = 
-  #avg_bike_speed   = average bike travel speed
+tour_assignment <- function(tmp_cut, tmp, max_weight, max_tour_length, max_tour_time, start_tour, avg_speed, hub) {
+  stats <- data.frame()
+  schedule <- data.frame()
+  for (k in 1:max(tmp_cut)) {
+    tmp_tour <- tmp[which(tmp_cut==k),]
+    if (all(is.na(tmp_tour$tourID))) { #cluster not yet assigned
+      if (sum(tmp_tour$weight) <= max_weight) { #tot cluster weight < weight capacity
+        
+        #select start_tour location (depot or hub)
+        if (hub=="N") start <- start_tour[,c("lon", "lat")] else start <- start_tour[start_tour$hubID==unique(tmp_tour$hubID),c("lon", "lat")] 
+          
+        #compute tour length and time
+        tour <- solve_tsp(tmp_tour, start)
+        tourlength <- tour$tour_length 
+        tourtraveltime <- tourlength/avg_speed 
+        tourdwelltime <- sum(tmp_tour$dwell_time)
+        tourtottime <- tourtraveltime + tourdwelltime
 
-  tmp$hub <- NA
+        if (tourlength <=  max_tour_length & tourtottime <= max_tour_time) { #tot tour length <= max allowed length & tot tour time <= max allowed time
+          tourID <- ifelse(all(is.na(tmp$tourID)), 1, max(tmp$tourID, na.rm=T)+1)
+          tmp[which(tmp_cut==k),"tourID"] <- tourID #assign cluster to tour ID
+          tour_stats <- data.frame("hubID"=ifelse(hub=="N", NA, unique(tmp_tour$hubID)),
+                                   "tourID"=tourID,
+                                   "tour_travel_distance"=tourlength,
+                                   "tour_travel_time"=tourtraveltime,
+                                   "tour_dwell_time"=tourdwelltime,
+                                   "tour_tot_time"=tourtottime,
+                                   "tour_ndeliveries"=nrow(tmp_tour),
+                                   "tour_n_express"=sum(tmp_tour$service_level %in% c("Express", "Express Plus")), stringsAsFactors = F)
+          stats <- rbind(stats, tour_stats)
+          tour_schedule <- data.frame("hubID"=ifelse(hub=="N", NA, unique(tmp_tour$hubID)),
+                                      "tourID"=tourID,
+                                      "nodeID"=tour$path_labels,
+                                      "tourSEQ"=1:length(tour$path_labels), stringsAsFactors = F)
+          schedule <- rbind(schedule, tour_schedule)
+        }
+      }
+    }
+  }
+  return(list("assignment"=tmp$tourID, "schedule"=schedule,"stats"=stats))
+  }
+
+
+
+
+
+solve_tsp <- function(tmp_tour, start) {
   
-  #leaflet(data=tmp) %>% #plot all delivery points for a given day
-  #  addProviderTiles("Stamen.TonerLite", options = providerTileOptions(noWrap = TRUE) ) %>%
-  #  addCircleMarkers(~lon, ~lat, stroke = FALSE, fillOpacity = 0.8, radius=5)
+  # distance matrix
+  dmat <- rbind(tmp_tour[,c("lon", "lat")], start[,c("lon", "lat")])
+  dmat <- distm(dmat)
+  dmat <- as.dist(dmat) #in meters
+  tsp <- TSP(dmat, labels=c(tmp_tour$nodeID, "start"))
+  
+  # solve TSP
+  tour <- solve_TSP(tsp, method="nearest_insertion")
+  START <- which(labels(tsp)=="start")
+  
+  return(list("path_labels"=c("hub", labels(cut_tour(tour, START)), "hub"), 
+              "tour_length"=tour_length(tour)))
+}
+  
+  #m <- as.matrix(tsp)
+  #hub <- which(labels(tsp)=="hub")
+  
+  #atsp <- ATSP(m[-hub,-hub])
+  #atsp <- insert_dummy(atsp, label="HUB")
+  #HUB <- which(labels(atsp)=="HUB")
+  #atsp[HUB,] <- c(m[-hub,hub], 0)
+  #atsp[,HUB] <- c(m[hub,-hub], 0)
+  
+  #resTOUR <- solve_TSP(atsp, method="nearest_insertion")
+  
+  #return(list("path_labels"=c("hub", labels(cut_tour(resTOUR, HUB)), "hub"), 
+  #            "tour_length"=tour_length(resTOUR)))
+  
+
+
+
+
+
+day_scheduling <- function(tmp, depot_loc, max_weight, max_tour_length, max_tour_time, avg_speed, hub, max_nboxes_hub, hub_cut) {
+###schedule delivery tours for bikes and assign them to hubs 
+  #tmp              = demand for single day
+  #depot_loc
+  #max_weight
+  #max_tour_length
+  #avg_speed
+  #hub
+  #max_nboxes_hub
+  #hub_cut
+###-------------------------------------
+
+  RESULTS <- list()
   
   # distance matrix
   dmat <- tmp[,c("lon", "lat")] 
@@ -92,63 +145,62 @@ day_scheduling <- function(tmp, max_nboxes_hub, boxweight, bike_tour_length, hub
   hc <- hclust(dmat, method="complete")
   
   # hub assignment
-  tmp$hub <- tmp_cut <- cutree(hc, h=hub_cut)
-  hub_centers <- merge(aggregate(lat ~ hub, data = tmp, FUN = mean), aggregate(lon ~ hub, data = tmp, FUN = mean))
-  hub_centers$date <- unique(tmp$date)
+  if (hub=="Y") {
+    tmp$hubID <- tmp_cut <- cutree(hc, h=hub_cut)
+    hub_centers <- merge(aggregate(lat ~ hubID, data = tmp, FUN = mean), aggregate(lon ~ hubID, data = tmp, FUN = mean))
+    start_tour <- hub_centers
+  } else {
+    tmp_cut <- rep(1, nrow(tmp))
+    start_tour <- depot_loc
+  }
   
   # tour assignment
   tmp$tourID <- NA
+  tour_schedule <- data.frame()
+  tour_stats <- data.frame()
   while (any(is.na(tmp$tourID))) {
-    tmp$tourID <- bike_assignment(tmp_cut, tmp, boxweight, bike_tour_length, hub_centers)
+    tour <- tour_assignment(tmp_cut, tmp, max_weight, max_tour_length, max_tour_time, start_tour, avg_speed, hub)
+    tmp$tourID <- tour$assignment
+    tour_schedule <- rbind(tour_schedule, tour$schedule)
+    tour_stats <- rbind(tour_stats, tour$stats)
     #print(max(tmp_cut))
     if (max(tmp_cut)<nrow(tmp)) tmp_cut <- cutree(hc, k=max(tmp_cut)+1) else break
   }
+  tour_schedule$date <- tour_stats$date <- unique(tmp$date)
   
-  # compute hub refill trips and respective travel distance and time from depot
-  hub_centers$trips <- aggregate(tourID ~ hub, data=tmp, FUN= function(x) {return(ceiling(length(unique(x))/max_nboxes_hub))})[,2]
-  hub_centers$depot_dist <- apply(hub_centers[,c("lon", "lat")], MARGIN=1, FUN= function(x){distm(x, depot_loc)}) #meters
-  hub_centers$tot_travel_dist <- hub_centers$depot_dist*hub_centers$trips*2 #meters
-  hub_centers$tot_trave_time <- hub_centers$tot_travel_dist/avg_truck_speed #minutes
- 
-  
-  # TSP
-  day_tour_schedule <- data.frame()
-  day_tour_stats <- data.frame()
-  tmp$tourID <- as.character(tmp$tourID)
-  for (j in 1:length(unique(tmp$tourID))) {
-    tmp_tour <- tmp[tmp$tourID==unique(tmp$tourID)[j],]
-    if (nrow(tmp_tour)>1) tour_tsp <- bike_tsp(tmp_tour, hub_centers) else tour_tsp <- list("path_labels"=tmp_tour$nodeID, "tour_length"=0)
+  # hub trips
+  if (hub=="Y") {
+    hub_centers$date <- unique(tmp$date)
     
-    # save results
-    tour_schedule <- data.frame("date"=unique(tmp_tour$date),
-                                "hubID"=unique(tmp_tour$hub),
-                                "tourID"=j,
-                                "nodeID"=tour_tsp$path_labels,
-                                "tourSEQ"=1:length(tour_tsp$path_labels), stringsAsFactors = F
-                                )
-    tour_stats <- data.frame("date"=unique(tmp_tour$date),
-                             "hubID"=unique(tmp_tour$hub),
-                             "tourID"=j,
-                             "tour_travel_distance"=tour_tsp$tour_length,
-                             "hub_travel_distance"=(nrow(hub_centers)+sum(hub_centers$refill_trips))*18000*2,
-                             "tour_travel_time"=(tour_tsp$tour_length/avg_bike_speed),
-                             "hub_travel_time"=(nrow(hub_centers)+sum(hub_centers$refill_trips))*20*2,
-                             "tour_dwell_time"=sum(tmp_tour$dwell_time),
-                             "tour_no_deliveries"=nrow(tmp_tour),
-                             "tour_no_express"=sum(tmp_tour$service_level!="Saver"), stringsAsFactors = F
-                             )
-    
-    day_tour_schedule <- rbind(day_tour_schedule, tour_schedule)
-    day_tour_stats <- rbind(day_tour_stats, tour_stats)
+    # compute hub refill trips and respective travel distance and time from depot
+    hub_centers$trips <- aggregate(tourID ~ hubID, data=tmp, FUN= function(x) {return(ceiling(length(unique(x))/max_nboxes_hub))})[,2]
+    hub_centers$depot_dist <- apply(hub_centers[,c("lon", "lat")], MARGIN=1, FUN= function(x){distm(x, depot_loc)}) #meters
+    hub_centers$tot_travel_dist <- hub_centers$depot_dist*hub_centers$trips*2 #meters
+    hub_centers$tot_trave_time <- hub_centers$tot_travel_dist/750 #minutes (750 is the avg truck speed in meters/min --CHECK!)
+    RESULTS[["day_hub"]] <- hub_centers
   }
+  
+  RESULTS[["day_tour_schedule"]] <- tour_schedule
+  RESULTS[["day_tour_stats"]] <- tour_stats
+  
+  return(RESULTS)
   
   ### FLEET SIZE
   #summary_tours_day <- unique(day_res_tour[,c("date", "hub", "tourID", "tour_travel_distance", "tour_travel_time", "tour_dwell_time", "tour_no_express")])
   #summary_tours_day <- summary_tours_day[order(-summary_tours_day$tour_no_express),]
   #summary_tours_day$tot_tour_time <- summary_tours_day$tour_travel_time + summary_tours_day$tour_dwell_time
   
-  return(list("day_hub"=hub_centers, "day_tour_schedule"=day_tour_schedule, "day_tour_stats"=day_tour_stats))
-  
 }
+
+
+
+
+
+
+
+#leaflet(data=tmp_tour) %>% #plot all delivery points for a given day
+#  addProviderTiles("Stamen.TonerLite", options = providerTileOptions(noWrap = TRUE) ) %>%
+#  addCircleMarkers(~lon, ~lat, stroke = FALSE, fillOpacity = 0.8, radius=5) %>%
+#  addPolylines(data=results_bikes[results_bikes$tourID==i,], lat = ~lat, lng = ~lon, weight=2, color = "red") 
 
 
--- R/cargobikes_main.R
+++ R/cargobikes_main.R
@@ -1,3 +1,7 @@
+
+# This code simulates two urban goods distribution systems: (1) truck-based delivery and (2) mobile depot-based delivery with cargo bikes
+# Both systems faces a given demand scenario (set of selivery points located in the CBD of Singapore)
+
 rm(list=ls())
 
 library(ggplot2)
@@ -7,134 +11,112 @@ library(geosphere)
 library(dplyr)
 library(TSP)
 
-dir <- "/Users/giacomodc/Dropbox/cargobikes_study"
-setwd(dir)
-source(paste0(dir,"/R/cargobikes_fn.R"))
+input_data <- "data/raw.csv"
+source(paste0(getwd(),"/R/cargobikes_fn.R"))
 
 
-### LOAD
-raw <- read.csv("data/raw.csv", stringsAsFactors = F, header=T)
-names(raw) <- c("month", "weekend_date", "date", "pkg_barcode_num", "stop_start_time", "stop_end_time", "lat", "lon", "commercial_residential", "dispatch_loop_position_num", "stop_type", "consignee_address1","consignee_address2","consignee_address3", "consignee_postcode","service_level_code", "service_level", "weight", "delivery_info", "dwell_time")
-
-
-### CLEAN
+### INPUT DATA
+raw <- read.csv(input_data, stringsAsFactors = F, header=T)
+names(raw) <- c("month", "weekend_date", "date", "pkg_barcode_num", "stop_start_time", "stop_end_time", "lat", "lon", "commercial_residential", 
+                "dispatch_loop_position_num", "stop_type", "consignee_address1","consignee_address2","consignee_address3", "consignee_postcode",
+                "service_level_code", "service_level", "weight", "delivery_info", "dwell_time")
 raw$stop_start_time <- as.POSIXct(paste(raw$date, "00:00:00"), format = "%d/%m/%y %H:%M:%S") + 3600*raw$stop_start_time #needed only to compute dwell time
 raw$stop_end_time <- as.POSIXct(paste(raw$date, "00:00:00"), format = "%d/%m/%y %H:%M:%S") + 3600*raw$stop_end_time #needed only to compute dwell time
 raw$dwell_time <- raw$dwell_time*60 #convert hours to minutes
 raw$date <- as.Date(raw$date, format="%d/%m/%y")
 raw$weight <- round(raw$weight, 1)
-raw <- raw[raw$weight<180,] #suspect one outlier with weight==256.8 ...CHECK!
-raw$nodeID <- 1:nrow(raw)
-raw$nodeID <- as.character(raw$nodeID)
+raw$nodeID <- as.character(1:nrow(raw))
 
 
-### INPUTS
-max_nboxes_hub <- 9 #max number of boxes carried by hub
+
+### INPUTS PARAMETERS
+# depot
+depot_loc <- data.frame("lon"=103.967465, "lat"=1.331026) # 22 Changi South Ave 2, UPS House Singapore, Singapore
+
+# cargo bike
 max_boxweight <- 205 #180 kg in the box + 25 kg front bike
 tot_load_share <- 0.7 #70% of max filling
-boxweight <- max_boxweight*tot_load_share #max weight a bike-tour can carry
-bike_tour_length <- 40000 #(in meters) max bike tour length is 50km, we assume 40 km since we use euclidean distances 
-hub_cut <- 2000 #(in meters) max dist allowed betwwen any 2 points within same hub cluster
-avg_bike_speed <- 250 #(meters/min) average bike speed
-bike_dwelltime_frac <- 0.6 #if ==1 then the dwell time bike == dwell time truck; if with bike the dwell time is reduced by 50%, then bike_dwelltime_frac <- 0.5
-avg_truck_speed <- 750 #(meters/min) average truck speed (==45 km/h)
-depot_loc <- c(103.967465, 1.331026) # (lon, lat) of depot
-mode <- "bike" #truck
+boxweight <- max_boxweight*tot_load_share #max weight carried
+bike_max_tour_length <- 40000 #(in meters) max bike tour length is 50km, we assume 40 km since we use euclidean distances
+bike_max_tour_time <- 10*60 #minutes
+bike_avg_speed <- 250 #(meters/min) average bike speed
+bike_dwelltime <- 0.6 #if ==1 then the dwell time bike == dwell time truck; if with bike the dwell time is reduced by 50%, then bike_dwelltime_frac <- 0.5
 
-### SCENARIOS
-nrep = 40 #no. of simulations for each scenario (==number of days simulated)
-#vector_xweight = seq(from=0.5, to=4, by=0.25)
-#vector_xweight = 1
-vector_xweight = seq(from=0.5, to=2, by=0.5)
-#vector_ndel = seq(from=50, to=1000, by=50)
-vector_ndel = seq(from=100, to=500, by=100)
+# hub
+max_nboxes_hub <- 9 #max number of boxes carried by hub
+hub_cut <- 2000 #(in meters) max dist allowed betwwen any 2 points within same hub cluster
+
+# truck
+truck_avg_speed <- 750 #(meters/min) average truck speed (==45 km/h)
+truck_max_weight <- 600 #kg
+truck_max_tour_length <- 500*1000 #meters
+truck_max_tour_time <- 10*60 #minutes
+
+# demand scenarios
+nrep = 2 #no. of days to simulate (per each scenario)
+vector_xweight = seq(from=0.5, to=4, by=0.25)
+#vector_xweight = c(0.5,1)
+vector_ndel = seq(from=50, to=1000, by=50)
+#vector_ndel = c(100, 300)
 scenarios <- data.frame("ndel"=rep(vector_ndel, each=length(vector_xweight)), "xweight"=vector_xweight)
 
-res_ALLscenarios_hub <- data.frame()
-res_ALLscenarios_tour_scehdule <- data.frame()
-res_ALLscenarios_tour_stats <- data.frame()
+
+
+### RUN
+res_ALLscenarios_bike <- list()
+res_ALLscenarios_truck <- list()
 
 for (k in 1:nrow(scenarios)) {
+  
   print(k)
+  
+  # generate demand scenarios
   xweight <- scenarios[k,"xweight"]
   ndel <- scenarios[k,"ndel"]
-  
-  ### DEMAND GENERATION
   dat <- generate_demand(data=raw, xweight=xweight, simulated="Y", ndel=ndel, nrep=nrep) # original data contains 64 days, ~300 deliveries per day
-  if (mode=="bike") dat$dwell_time <- dat$dwell_time*bike_dwelltime_frac
   
-  ### SCHEDULING
-  res_hub <- data.frame()
-  res_tour_schedule <- data.frame()
-  res_tour_stats <- data.frame()
+  # perform scheduling
+  res_bike <- list("hub"=data.frame(), "tour_schedule"=data.frame(), "tour_stats"=data.frame())
+  res_truck <- list("tour_schedule"=data.frame(), "tour_stats"=data.frame())
+ 
   for (i in 1:length(unique(dat$date))) {
-    #print(i)
     
     # select day
     tmp <- dat[dat$date==unique(dat$date)[i],]
     
-    while (any(tmp$weight>boxweight)) { #if the weight of a single delivery exceed the capacity of a cargo bike, the deliverty point is split in 2
-      excess_weight <- tmp[tmp$weight>boxweight,"weight"][1] - boxweight #find excess weight
-      tmp[nrow(tmp)+1,] <- tmp[tmp$weight>boxweight,][1,] #add new delivery point
-      tmp[tmp$weight>boxweight,"weight"][1] <- boxweight #adjust original del point to max capacity
-      tmp[nrow(tmp),"weight"] <- excess_weight #adjust weight of new delivery point
-    }
+    # mode: bike
+    tmp_adj <- adjust_demand(tmp, dwell_factor=bike_dwelltime, max_weight=boxweight) #adjust demand
+    res <- day_scheduling(tmp=tmp_adj, depot_loc=depot_loc, max_weight=boxweight, max_tour_length=bike_max_tour_length, max_tour_time=bike_max_tour_time, avg_speed=bike_avg_speed, hub="Y", max_nboxes_hub, hub_cut) #simulate bikes
+    res_bike$hub <- rbind(res_bike$hub, res$day_hub)
+    res_bike$tour_schedule <- rbind(res_bike$tour_schedule, res$day_tour_schedule)
+    res_bike$tour_stats <- rbind(res_bike$tour_stats, res$day_tour_stats)
     
+    # mode: truck
+    tmp_adj <- adjust_demand(tmp, dwell_factor=1, max_weight=truck_max_weight)
+    res <- day_scheduling(tmp=tmp_adj, depot_loc=depot_loc, max_weight=truck_max_weight, max_tour_length=truck_max_tour_length, max_tour_time=truck_max_tour_time, avg_speed=truck_avg_speed, hub="N")
+    res_truck$tour_schedule <- rbind(res_truck$tour_schedule, res$day_tour_schedule)
+    res_truck$tour_stats <- rbind(res_truck$tour_stats, res$day_tour_stats)
     
-    # compute day schedule
-    res <- day_scheduling(tmp, max_nboxes_hub, boxweight, bike_tour_length, hub_cut, avg_bike_speed, depot_loc)
-    
-    res_hub <- rbind(res_hub, res$day_hub)
-    res_tour_schedule <- rbind(res_tour_schedule, res$day_tour_schedule)
-    res_tour_stats <- rbind(res_tour_stats, res$day_tour_stats)
   }
   
-  res_hub$xweight <- xweight
-  res_hub$ndel <- ndel
-  res_hub$scenario <- k
-  res_tour_schedule$xweight <- xweight
-  res_tour_schedule$ndel <- ndel
-  res_tour_schedule$scenario <- k
-  res_tour_stats$xweight <- xweight
-  res_tour_stats$ndel <- ndel
-  res_tour_stats$scenario <- k
+  # add demand scenario features
+  res_bike <- mapply(cbind, res_bike, "xweight"=xweight, SIMPLIFY=F)
+  res_bike <- mapply(cbind, res_bike, "ndel"=ndel, SIMPLIFY=F)
+  res_bike <- mapply(cbind, res_bike, "scenario"=k, SIMPLIFY=F)
+  res_truck <- mapply(cbind, res_truck, "xweight"=xweight, SIMPLIFY=F)
+  res_truck <- mapply(cbind, res_truck, "ndel"=ndel, SIMPLIFY=F)
+  res_truck <- mapply(cbind, res_truck, "scenario"=k, SIMPLIFY=F)
   
-  res_ALLscenarios_hub <- rbind(res_ALLscenarios_hub, res_hub)
-  res_ALLscenarios_tour_scehdule <- rbind(res_ALLscenarios_tour_scehdule, res_tour_schedule)
-  res_ALLscenarios_tour_stats <- rbind(res_ALLscenarios_tour_stats, res_tour_stats)
-  
+  # save results
+  res_ALLscenarios_bike[[k]] <- res_bike
+  res_ALLscenarios_truck[[k]] <- res_truck
 }
 
 
+
 ### SAVE
-
-res_ALLscenarios_hub[,] <- lapply(res_ALLscenarios_hub[,], as.character)
-write.csv(res_ALLscenarios_hub, file="results/simulated/res_hub.csv", row.names =F)
-
-res_ALLscenarios_tour_scehdule[,] <- lapply(res_ALLscenarios_tour_scehdule[,], as.character)
-write.csv(res_ALLscenarios_tour_scehdule, file="results/simulated/res_tour.csv", row.names =F)
-
-res_ALLscenarios_tour_stats[,] <- lapply(res_ALLscenarios_tour_stats[,], as.character)
-write.csv(res_ALLscenarios_tour_stats, file="results/simulated/res_tourstats.csv", row.names =F)
-
-
-#map_orders <- leaflet(data=tmp) %>% #plot all delivery points for a given day
-#  addProviderTiles("Stamen.TonerLite", options = providerTileOptions(noWrap = TRUE) ) %>%
-#  addCircleMarkers(~lon, ~lat, stroke = FALSE, fillOpacity = 0.8, radius=5)
-#for (i in 1:max(results_tours$tourID)) {
-#  map_orders <- map_orders %>% addPolylines(lat = results_tours[results_tours$tourID==i,"lat"], lng = results_tours[results_tours$tourID==i,"lon"]) 
-#}
-#map_orders
-
-
-
-
-
-
-
-
-
-
+save(res_ALLscenarios_bike, file="results/res_bike.RData")
+save(res_ALLscenarios_truck, file="results/res_truck.RData")
 
 
 
